96a6aafd0549094e0c59bc34ac2233bd67f9b9f7
REMI DUBUS <remdubus@gmail.com>
2024-10-31 17:35:27 +0100

    feat: move de alert to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import pokemonListData from "./assets/data/pokemonListData.json";
 import NavBar from "./components/NavBar";
 import PokemonCard from "./components/PokemonCard";
@@ -9,6 +9,11 @@ import type { PokemonProps } from "./lib/defintions";
 function App() {
 	const [pokemonIndex, setPokemonIndex] = useState(0);
 	const pokemonList: PokemonProps[] = pokemonListData;
+
+	useEffect(() => {
+		alert("hello pokemon trainer :)");
+	}, []);
+
 	if (pokemonIndex === 3) {
 		alert("pika pikachu !!!");
 	}
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,6 @@
-import { useEffect } from "react";
 import type { PokemonProps } from "../lib/defintions";
 
 const PokemonCard = ({ name, imgSrc }: PokemonProps) => {
-	useEffect(() => {
-		alert("hello pokemon trainer :)");
-	}, []);
 	return (
 		<figure>
 			{imgSrc ? <img src={imgSrc} alt={name} /> : <p>???</p>}
